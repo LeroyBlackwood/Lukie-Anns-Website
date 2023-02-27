c964403c3d8edff2259613203cd426e620f8805a
Leroy Blackwood <blackwoodleroy04@gmail.com>
2023-02-26 20:51:45 -0500

    update

--- javascript/script.js
+++ javascript/script.js
@@ -12,31 +12,5 @@ function slide() {
   myIndex++;
   if (myIndex > images.length) {myIndex = 1}    
   images[myIndex-1].style.display = "block";  
-  setTimeout(slide, 5000); // Change image every 5 seconds
+  setTimeout(slide, 2000); // Change image every 2 seconds
 }
-
-const hamburger = document.querySelector(".hamburger");
-const x = document.querySelector(".x");
-const nav_menu = document.querySelector(".nav_menu");
-const nav_item = document.querySelectorAll(".nav_item");
-
-hamburger.addEventListener("click", () => {
-    hamburger.classList.toggle("active");
-    x.classList.toggle("active");
-    nav_menu.classList.toggle("active");
-
-})
-
-x.addEventListener("click", () => {
-    hamburger.classList.remove("active");
-    x.classList.remove("active");
-    nav_menu.classList.remove("active");
-})
-nav_item.forEach(n => n.addEventListener("click", () => {
-    x.classList.remove("active");
-    nav_menu.classList.remove("active");
-    hamburger.classList.remove("active");
-}))
-
-
-
