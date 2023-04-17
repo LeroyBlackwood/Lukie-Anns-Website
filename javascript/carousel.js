8e7bbbd2d7c3ad0a1a090569fd4faa9a65ebe143
Leroy Blackwood <blackwoodleroy04@gmail.com>
2023-04-17 01:01:35 -0500

    update

--- javascript/carousel.js
+++ javascript/carousel.js
@@ -1,17 +1,19 @@
-
-
-var myIndex = 0;
-slide();
-
-function slide() {
-  var i;
-  var images = document.getElementsByClassName("images");
-  for (i = 0; i < images.length; i++) {
-    images[i].style.display = "none";  
-  }
+$(document).ready(function(){
   
-  myIndex++;
-  if (myIndex > images.length) {myIndex = 1}    
-  images[myIndex-1].style.display = "block";  
-  setTimeout(slide, 5000); // Change image every 2 seconds
-}
+  var myIndex = 0;
+    var i;
+    var images = $(".images");
+    slide();
+
+    
+    function slide() {
+        for (i = 0; i < images.length; i++) {
+          images[i].style.display = "none";  
+        }
+        myIndex++;
+        if (myIndex > images.length) {myIndex = 1}    
+        images[myIndex-1].style.display = "block"; 
+        setTimeout(slide, 2000) 
+         }
+
+})
